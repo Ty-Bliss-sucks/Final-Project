76e5315268daea0ed8721d69f9423c3a127f71f9
THE-GREYS <114932648+Ty-Bliss-sucks@users.noreply.github.com>
2023-05-17 10:31:47 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+window.addEventListener("DOMContentLoaded", function(e) {
+
+    // Original JavaScript code by Chirp Internet: chirpinternet.eu
+    // Please acknowledge use of this code by including this header.
+
+    var stage = document.getElementById("stage");
+    var fadeComplete = function(e) { stage.appendChild(arr[0]); };
+    var arr = stage.getElementsByTagName("a");
+    for(var i=0; i < arr.length; i++) {
+      arr[i].addEventListener("animationend", fadeComplete, false);  Selected_element.id = newID;
+    }
+window.addEventListener("DOMContentLoaded", function(e) {
+
+    // Original JavaScript code by Chirp Internet: chirpinternet.eu
+    // Please acknowledge use of this code by including this header.
+
+    var stages = document.getElementById("stages");
+    var fadeComplete = function(e) { stage.appendChild(arr[0]); };
+    var arrr = stage.getElementsByTagName("a");
+    for(var i=0; i < arrr.length; i++) {
+      arrr[i].addEventListener("animationend", fadeComplete, false);  Selected_element.id = newID;
+    }
